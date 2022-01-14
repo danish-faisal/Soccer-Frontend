b742d93a3289f808b060f6da46cbbfc78591023d
= <danishfaisal.wwe@gmail.com>
2022-01-14 18:42:12 +0530

    Add the main page structure, the state and axios

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import PlayerList from './Player/PlayerList';
+import PlayerSingle from './Player/PlayerSingle';
+import PlayerForm from './Player/PlayerForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {}
+    }
+
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:4000/players';
+
+    axios.get(url)
+      .then(Response => {
+        this.setState({
+          players: Response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  updateCurrentPlayer(item) {
+    this.setState({
+      currentPlayer: item
+    });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid" >
+        <div className="row">
+          <div className="col s12">Menu</div>
+        </div>
+        <div className="row">
+          <div className="col s3">
+            <PlayerList player={this.state.players} updateCurrentPlayer={this.updateCurrentPlayer} />
+          </div>
+          <div className="col s9"><PlayerSingle /></div>
+        </div>
+        <div className="row">
+          <div className="col s12"><PlayerForm /></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
