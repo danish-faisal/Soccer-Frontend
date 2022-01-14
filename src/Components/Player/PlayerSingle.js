9ea857dae48155f584616d764996a1d12ac048c4
= <danishfaisal.wwe@gmail.com>
2022-01-14 21:02:44 +0530

    Create the single item component

--- src/Components/Player/PlayerSingle.js
+++ src/Components/Player/PlayerSingle.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-const PlayerSingle = () => {
+const PlayerSingle = (props) => {
     return (
-        <div>
-            PlayerSingle
+        <div className="row">
+            <div className="col s12">
+                <div className="card">
+                    <div className="card-image">
+                        <img src="soccer.jpg" />
+                        <span className="card-title">{props.player.firstName} {props.player.lastName}</span>
+                    </div>
+                    <div className="card-content">
+                        <p>Phone: {props.player.phone} - Email: {props.player.email}</p>
+                        <p>Strenght: {props.player.strength} - Endurance: {props.player.endurance}</p>
+                    </div>
+                    <div className="card-action">
+                        <p>Team: {props.player.team}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
