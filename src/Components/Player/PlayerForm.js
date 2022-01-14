35b0d19ea96688b52b217f48f39f0de46a7294cd
= <danishfaisal.wwe@gmail.com>
2022-01-14 22:25:08 +0530

    Finalize the form

--- src/Components/Player/PlayerForm.js
+++ src/Components/Player/PlayerForm.js
@@ -1,33 +1,53 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PlayerForm extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.firstName = React.createRef();
+        this.lastName = React.createRef();
+        this.phone = React.createRef();
+        this.email = React.createRef();
+    }
+
+    submitPlayer(event) {
+        event.preventDefault();
+
+        axios.post('http://localhost:4000/players', {
+            firstName: this.firstName.current.value,
+            lastName: this.lastName.current.value,
+            phone: this.phone.current.value,
+            email: this.email.current.value,
+        }).then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+        });
     }
 
     render() {
         return (
-            <div class="row">
+            <div className="row">
                 <h1>Add a new player</h1>
-                <form class="col s12" onSubmit={this.submitPlayer.bind(this)}>
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <input id="firstName" ref="firstName" type="text" />
+                <form className="col s12" onSubmit={this.submitPlayer.bind(this)}>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="firstName" ref={this.firstName} type="text" />
                             <label htmlFor="firstName">First Name</label>
                         </div>
-                        <div class="input-field col s6">
-                            <input id="lastName" ref="lastName" type="text" />
+                        <div className="input-field col s6">
+                            <input id="lastName" ref={this.lastName} type="text" />
                             <label htmlFor="lastName">Last Name</label>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <input id="phone" ref="phone" type="text" />
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input id="phone" ref={this.phone} type="text" />
                             <label htmlFor="phone">Phone</label>
                         </div>
-                        <div class="input-field col s6">
-                            <input id="email" ref="email" type="text" />
+                        <div className="input-field col s6">
+                            <input id="email" ref={this.email} type="text" />
                             <label htmlFor="email">Email</label>
                         </div>
                     </div>
